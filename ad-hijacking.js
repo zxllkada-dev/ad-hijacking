d0c3857caf434ea784d97777a04ce8b3af98d76d
Kada <103851762+zxllkada-dev@users.noreply.github.com>
2025-12-31 21:58:47 +0100

    Update ad-hijacking.js

--- ad-hijacking.js
+++ ad-hijacking.js
@@ -1,7 +1,1 @@
-<script>
-setTimeout(function() {
-    var ad = document.createElement('div');
-    ad.innerHTML = '<div style="position:fixed;bottom:20px;right:20px;background:blue;color:white;padding:20px;border-radius:10px;z-index:99998">Your Ad Here</div>';
-    document.body.appendChild(ad);
-}, 3000);
-</script>
+
